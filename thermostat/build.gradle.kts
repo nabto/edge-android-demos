10925116c52c386a57e6f545433a889081329730
Ahmad Saleh <ahmadsaleh@quark.dev>
2022-11-24 16:26:41 +0100

    Change version names to 1.1

--- thermostat/build.gradle.kts
+++ thermostat/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 32
         versionCode = 11
-        versionName = "1.0"
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
